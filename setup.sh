1516b855f59c2e2bc5160e5db75c3cce6d8cf71b
Michael Knox <mknox@assemblypayments.com>
2020-03-11 07:55:36 +1100

    remove need for py env activate

--- setup.sh
+++ setup.sh
@@ -10,13 +10,10 @@ cd "${_source_dir}" || exit 1
 echo "Setup Python virtual env"
 python3 -m venv venv
 
-# shellcheck disable=SC1091
-source ./venv/bin/activate
-
 echo "Install requirements"
-pip3 install -r bk_ssm_secrets/requirements.txt
+venv/bin/pip3 install -r bk_ssm_secrets/requirements.txt
 
 echo "and add the bk_ssm_secrets package"
-pip3 install -e bk_ssm_secrets
+venv/bin/pip3 install -e bk_ssm_secrets
 
 cd "${_current_dir}" || exit
