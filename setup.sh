28fdb18c5219430335f4da71adc6f0eabc3a770e
Michael Knox <mknox@assemblypayments.com>
2020-03-11 21:41:53 +1100

    remove pyth v env

--- setup.sh
+++ setup.sh
@@ -7,17 +7,10 @@ _current_dir="$(pwd)"
 
 cd "${_source_dir}" || exit 1
 
-echo "Setup Python virtual env"
-python3 -m venv venv
-
-export VIRTUAL_ENV_DISABLE_PROMPT=true
-# shellcheck disable=SC1091
-source "./venv/bin/activate"
-
 echo "Install requirements"
-venv/bin/pip3 install -r bk_ssm_secrets/requirements.txt
+pip3 install -r bk_ssm_secrets/requirements.txt
 
 echo "and add the bk_ssm_secrets package"
-venv/bin/pip3 install -e bk_ssm_secrets
+pip3 install -e bk_ssm_secrets
 
 cd "${_current_dir}" || exit
