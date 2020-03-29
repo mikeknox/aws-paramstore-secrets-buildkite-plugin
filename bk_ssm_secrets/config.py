cbc581298454429b2342b9ed5cee48e166f1e903
Kai Xia <kaix+github@fastmail.com>
2020-03-29 12:37:21 +1100

    Cleanup in Python space.
    1. Create three BkSecrets instances, each represents a sub namespace,
      this will help when there're a lot of items in parameter store, as the
      old implementation will read all of them.
    2. split `get_secrets` into `parse_env` and `parse_ssh`.
    3. move ssh agent logic in bksecrets.py into helpers.py.
    4. Ignore project level and top(global) level ssh keys.
    5. Improve logging.
    6. remove SECRET_TYPES and MODE from config
    7. add `key_to_env_name` function in helper to properly format the env
      name. For example, `bitbucket-user` will become `BITBUCKET_USER`.
    8. Simplify `dump_env_secrets`.
    
    Signed-off-by: Kai Xia <kaix+github@fastmail.com>

--- bk_ssm_secrets/config.py
+++ bk_ssm_secrets/config.py
@@ -2,15 +2,8 @@ import os
 
 
 BASE_PATH = os.environ.get(
-    "BUILDKITE_PLUGIN_AWS_PARAMSTORE_SECRETS_PATH", "/vendors/buildkite/secrets"
+    "AWS_PARAMSTORE_SECRETS_PATH", "/vendors/buildkite/secrets"
 )
 DEFAULT_SLUG = os.environ.get(
-    "BUILDKITE_PLUGIN_AWS_PARAMSTORE_SECRETS_DEFAULT_KEY", "global"
+    "AWS_PARAMSTORE_SECRETS_DEFAULT_KEY", "global"
 )
-# Return an list of the secret types that we can retrieve.
-# This is derived from env var BUILDKITE_PLUGIN_AWS_PARAMSTORE_SECRETS_TYPES.
-# which is a colon delimited list.
-SECRET_TYPES = os.environ.get(
-    "BUILDKITE_PLUGIN_AWS_PARAMSTORE_SECRETS_TYPES", "env:ssh:git-creds"
-).split(":")
-MODE = os.environ.get("BUILDKITE_PLUGIN_AWS_PARAMSTORE_SECRETS_MODE", "pipeline")
--- bk_ssm_secrets/helpers.py
+++ bk_ssm_secrets/helpers.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import shlex
+import subprocess
 from urllib.parse import urlparse
 
 
@@ -22,6 +23,10 @@ def setup_logging():
     else:
         logging.basicConfig(level=logging.INFO, **logging_kwargs)
 
+
+setup_logging()
+
+
 def extract_ssh_agent_envars(agent_output):
     '''
     Parse the output from ssh-agent to get only the variables and values
@@ -41,23 +46,17 @@ def extract_ssh_agent_envars(agent_output):
     pairs = [line.split(";")[0] for line in agent_output.decode().splitlines()[:2]]
     return {pair.split("=")[0]: pair.split("=")[1] for pair in pairs}
 
-
 def dump_env_secrets(env_before):
-    # Get difference in sets
-    for key in set(os.environ) & set(env_before):
-        if os.environ[key] != env_before[key]:
-            if key == 'SSH_AGENT_PID' or key == 'SSH_AUTH_SOCK':
-                export = f"{key}={shlex.quote(os.environ[key])}"
-            else:
-                export = f"export {key}={shlex.quote(os.environ[key])}"
+    """
+    Diff the current os.environ with env_before to find changed environment
+    variables.
+    """
+    for key in os.environ:
+        changed_value = key in env_before and os.environ[key] != env_before[key]
+
+        if changed_value or key not in env_before:
             logging.debug(f"exporting: {export}")
-            print(export)
-    for key in set(os.environ) - set(env_before):
-        if key == 'SSH_AGENT_PID' or key == 'SSH_AUTH_SOCK':
-            export = f"{key}={shlex.quote(os.environ[key])}"
-        else:
             export = f"export {key}={shlex.quote(os.environ[key])}"
-        logging.debug(f"exporting: {export}")
         print(export)
 
 def url_to_slug(url):
@@ -71,3 +70,41 @@ def url_to_slug(url):
     if parsed.path:
         slug += "_" + parsed.path.strip("/").replace("/", "_").replace("~", "_")
     return slug
+
+def key_to_env_name(key_name):
+    return key_name.upper().replace("-", "_")
+
+def start_ssh_agent(ssh_key_text):
+    """
+    This function will start an SSH agent and add a ssh private key to it.
+
+    This is used to start a separate ssh agent for the ssh key we obtained from
+    parameter store.
+    """
+    ssh_agent = subprocess.run(
+        ['ssh-agent', '-s'], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    if ssh_agent.returncode != 0:
+        logging.error(
+            f"ssh-agent process failed with: {ssh_agent.returncode}."
+            f"stdout: `{ssh_agent.stdout}`, stderr: `{ssh_agent.stderr}`"
+        )
+        raise RuntimeError("starting ssh agent failed.")
+
+    envvars = os.environ.copy()
+    envvars.update(extract_ssh_agent_envars(ssh_agent.stdout))
+    logging.debug(f"Started new ssh-agent (pid {envvars['SSH_AGENT_PID']})")
+    envvars['SSH_ASKPASS'] = '/bin/false'
+    ssh_add = subprocess.run(
+        ['ssh-add', '-'], input=ssh_key_text + '\n', env=envvars,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8',
+    )
+
+    if ssh_add.returncode != 0:
+        logging.error(
+            f"ssh-add process failed with: {ssh_add.returncode}."
+            f"stdout: `{ssh_add.stdout}`, stderr: `{ssh_add.stderr}`"
+        )
+        raise RuntimeError("ssh-add failed.")
+
+    return envvars
